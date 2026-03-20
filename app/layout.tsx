e966bf96a18091a5e0ed828589499fc1c0b06105
s4ory <hoanghcm188@gmail.com>
2026-03-21 05:11:33 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "Lunaby API Docs",
     description: "Official developer documentation for Lunaby API",
+    icons: {
+        icon: "https://github.com/lun4by/Lunaby/blob/main/assets/avatar.png?raw=true",
+    },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
